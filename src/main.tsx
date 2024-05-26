2b41738ad8f3889d0bfbf44fc48c99478e78204e
Nickolas Zamachnoi <z.nickolas523@gmail.com>
2024-05-25 23:52:25 -0400

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import App from "./routes/App"
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+	},
+])
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+)
---
+++ src/routes/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+	return (
+		<div className="h-screen w-screen bg-violet-50 flex flex-row items-center justify-center">
+			<div className="grid grid-rows-2 grid-cols-1 text-center w-1/2">
+				<p className="font-semibold text-6xl text-black">Hi, I'm</p>
+				<h1 className="text-7xl font-bold text-violet-700">Nick Z</h1>
+			</div>
+			<div className="w-1/2 flex flex-row justify-center">
+				<p className="text-black">Random</p>
+			</div>
+		</div>
+	)
+}
+export default App
